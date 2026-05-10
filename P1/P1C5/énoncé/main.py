c8ccd59210ecebcc49991bc010b1da487082d03d
makhloufirayane38-sketch <makhloufirayane38@gmail.com>
2026-05-10 19:08:57 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom = "rayane"
+age = 18
+taille = "1.93m"
+est_etudient = true  
+print (f"nom : {nom}.")
+print (f"age : {age} . ")
+print (f"taille : {taille}.")
+print (f"etudient : {est_etudient}.")
+type(nom)
+type(age)
+type(taille)
+type(est_etudient)
