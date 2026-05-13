fd394b6096537b52f1b488a2beb6198a68163f46
Makhloufi Rayane Yacer <makhloufirayane38@gmail.com>
2026-05-13 15:38:24 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,12 @@
 # Écrivez votre code ici !
+fruits = ["pomme","banane","orange"]
+print(fruits)
+fruits.append("kiwi")
+print(fruits)
+fruits.remove("orange")
+print(fruits)
+fruits[1]="ananas"
+print(fruits)
+print(len(frtuits))
+sort(fruits)
+print(fruits)
