59e0751e84036d9a053ef0e2947d7acc3f79736e
Makhloufi Rayane Yacer <makhloufirayane38@gmail.com>
2026-05-15 11:33:07 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,8 @@
 # Écrivez votre code ici !
+fruits={"pomme":"rouge","banane":"jaune","orange":"orange"}
+fruits["kiwi"]="vert"
+fruits["banane"]
+couleur_banane=fruits["banane"]
+fruits["pomme"]="vert"
+del fruits["banane"]
+print(fruits)
