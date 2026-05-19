60f3d6eafdf4ffad8767c2a699eaa951956de5a1
Makhloufi Rayane Yacer <makhloufirayane38@gmail.com>
2026-05-19 12:22:46 +0100

    exos codition.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,32 @@
 # Ecrivez votre code ici !
+number1= input("enter the first number:")
+number2= input("enter the second number:")
+
+if not number1.isnumeric() or not number2.isnumeric():
+    print("error")
+    raise SystemExit("Fin du programme")
+
+number1= int(number1)
+number2= int(number2)
+
+operation = input("enter the operation you want to do (+, -, *, /):")
+
+if operation not in ["+", "-", "*", "/"]:
+    print("error")
+    raise SystemExit("Fin du programme")
+
+if operation == "+":
+    result = number1 + number2
+elif operation == "-":
+    result = number1 - number2
+elif operation == "*":
+    result = number1 * number2
+elif operation == "/":
+    if number2 ==0:
+        print("error: division by zero")
+        raise SystemExit("Fin du programme")    
+    else:
+        resul= number1 / number2
+      
+
+print(f"the result of the operation is : {round(result, 2)}")
